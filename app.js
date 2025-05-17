52fba92a51832b43e7655dd25a9e58340edcbe3c
thayour <temyking@gmail.com>
2025-05-17 12:07:45 +0100

    cron.js

--- app.js
+++ app.js
@@ -7,10 +7,13 @@ import {connectDB} from './database/db.js';
 import  authRoutes  from "./route/auth.js"
 import bookRoutes from './route/books.js';
 import cors from 'cors';
+import job from './database/cron.js';
 
 const app = express();
 const PORT = process.env.PORT
 
+
+job.start()
 app.use(cors())
 app.use(express.json())
 
