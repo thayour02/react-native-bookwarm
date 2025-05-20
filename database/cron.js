01e215448b3d0e6264199920a019a9e69013382d
thayour <temyking@gmail.com>
2025-05-20 19:24:52 +0100

    cron

--- database/cron.js
+++ database/cron.js
@@ -1,26 +1,20 @@
-import https from "https";
+import https from 'https';
 import cron from "cron";
 import "dotenv/config";
 
 
-const job = new cron.CronJob("*/14 * * * *", function () {
-  https
-    .get(process.env.API_URL, (res) => {
-      let data = "";
-      res.on("data", (chunk) => {
-        data += chunk;
-      });
-      res.on("end", () => {
-        if (res.statusCode === 200) {
-          console.log("GET request sent successfully");
-        } else {
-          console.log("Error in GET request", res.statusCode);
-        }
-      });
+
+
+const job = new cron.CronJob("*/14 * * * *", function() {
+    https
+    .get(process.env.API_URL, (res)=>{
+        if(res.statusCode === 200)console.log("GET request sent succesfully");
+        else console.log("Error in GET request", res.statusCode, res.statusMessage);
     })
-    .on("error", (e) => {
-      console.log("Error sending GET request", e);
-    });
-});
+    .on("error", (e)=>{
+        console.log("Error sending GET request", e);
+    })
+
+})
 
 export default job;
--- route/books.js
+++ route/books.js
@@ -5,10 +5,10 @@ import { authMiddleware } from '../middleware/authMidddleware.js';
 
 const router = express.Router();
 
-router.post("/create-book", authMiddleware, createBook);
+router.post("/", authMiddleware, createBook);
 
-router.get("/get-all-books", authMiddleware, getAllBooks);
-router.get("/get-book-by-user", authMiddleware, getBookbyUser);
+router.get("/", authMiddleware, getAllBooks);
+router.get("/user", authMiddleware, getBookbyUser);
 router.delete("/delete-book/:id", authMiddleware, deleteBook);
 
 export default router;
